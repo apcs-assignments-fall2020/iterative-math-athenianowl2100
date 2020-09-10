72ac0c01f9908702b054ed75518a705b75a8470a
athenianowl2100 <nathanlintest@gmail.com>
2020-09-10 19:10:42 +0000

    First draft

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,22 +4,40 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double x1 = 5;
+        double prev = 0;
+
+        while (Math.abs(prev - x1) > 0.0001) {
+            prev = x1;
+            x1 = (x1 + x/x1) / 2;
+        }
+
+        return x1;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double prod = 1;
+        while (x > 1) {
+            prod *= x;
+            x--;
+        }
+        return prod;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double e = 0;
+        int n = 0;
+
+        while (Math.abs(Math.E - e) > 0.00000000001) {
+            e += (1.0/factorial(n));
+            n++;
+        }
+
+        return e;
     }
     
     
@@ -27,9 +45,17 @@ public class MyMain {
         Scanner scan = new Scanner(System.in);
 
         // YOUR CODE HERE
+        System.out.println("Enter a positive number!");
+        double z = scan.nextDouble();
 
+        while (z <= 0) {
+            System.out.println("Enter a positive number!");
+            z = scan.nextDouble();
+        }
 
-        
+        System.out.println("The squareroot of " + z + " is " + babylonian(z));
+
+        System.out.println("The value of e is roughly : " + calculateE());
 
         scan.close();
     }
